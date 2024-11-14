fda4a3bd885202341520e766f7b94bdefc0ff62e
RomerX64 <tomiromera2903@gmail.com>
2024-11-14 19:06:55 -0300

    get Orders

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/users/users.module';
 import { ProductModule } from './modules/products/products.module';
+import { OrderModule } from './modules/orders/orders.module';
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [UserModule, ProductModule, OrderModule],
   controllers: [],
   providers: [],
 })
